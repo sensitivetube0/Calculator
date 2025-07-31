1c421390c8832d3c77b2b82cf24fe6fa8a533bd7
Jesse Collins <jessecollins0808@gmail.com>
2025-07-31 21:05:16 +0100

    Functioning calculator with no ability to use . and no keyboard support
    needed to make calculator functional before adding features to the calculator

--- calculator.js
+++ calculator.js
@@ -1,27 +1,26 @@
 let buttons = document.querySelectorAll("button");
-let allNo = ["1","2","3","4","5","7","8","9","0"];
+let allNo = ["1","2","3","4","5","6", "7","8","9","0"];
 let operaters = ["+","-","x","/"];
 let clearArr = ["clear"];
+let equalTo = ["="];
+let off = ["power"];
 let displayCount = 0;
 let display = document.querySelector("#display");
-let firstValue;
-let secondValue;
-let result;
+let firstNo;
+let firstValue = "";
+let secondValue = "";
+let operator = "";
+let result = "";
 if (displayCount<1){
 buttons.forEach(button => {
     button.addEventListener("click", () =>{
-        operation(button.id)
+        findValueAndOperator(button.id)
     })
 });
 }
-// if(displayCount=1){
-//     buttons.forEach(button => {
-//     button.addEventListener("click", () =>{
-//         operation2(button.id)
-//     })
-// });
-// }
+if (displayCount === 1){
 
+}
 
 function add(a , b){
     return a + b;
@@ -38,7 +37,7 @@ function divide(a , b){
 
 function operate(a , b , op){
     firstNo = a;
-    secondNo = b
+    secondNo = b;
     if(op === "+"){
       result = add(firstNo, secondNo);
     };
@@ -49,24 +48,124 @@ function operate(a , b , op){
         result = multiply(firstNo , secondNo);
     };
     if(op === "/"){
-        result = multiply(firstNo , secondNo);
+        result = divide(firstNo , secondNo);
     };
     return result;
 }
 
-function operation(a){
-    if(allNo.includes(a)){
+
+
+
+
+
+
+
+
+
+function findValueAndOperator(a){
+   if(off.includes(a)){
+    close();
+   }
+   
+   
+    if(clearArr.includes(a)){
+        displayCount = 0;
+        firstValue = ""
+        secondValue = ""
+        display.textContent = ""
+        result = ""
+       
+    }
+
+    if(allNo.includes(a) && (displayCount ===0)){ 
+        display.textContent = ""
         display.textContent = a;
         firstValue = a;
-        displayCount += 1;
+        displayCount = 1;
+        return;
     }
-    if (operaters.includes(a)){
+
+     if(allNo.includes(a) &&(displayCount ===1)){ 
+        
+        display.textContent += a;
+        firstValue += a;
+        displayCount = 1;
+        return;
+    }
+    
+
+
+    if (operaters.includes(a) && (displayCount === 0)){
         display.textContent = "ERROR"
+        return;
+    }
+    if (operaters.includes(a) && ((displayCount === 1) || displayCount === 1.5 )){
+        operator = a;
+        displayCount = 2
+        display.textContent = a
+        return;
+    }
+    
+    if(allNo.includes(a) && (displayCount ===2)){ 
+        display.textContent = ""
+        display.textContent = a;
+        secondValue = a;
+        displayCount = 3;
+        return;
+    }
+
+
+   
+    if(allNo.includes(a) &&(displayCount ===3)){ 
+        
+        display.textContent += a;
+        secondValue += a;
+        displayCount = 3;
+        return;
+     }
+   
+   
+   
+     if(equalTo.includes(a) &&( displayCount >= 3)){
+        finalNo = findValue(firstValue, secondValue, operator);
+        display.textContent = finalNo
+        firstValue = finalNo
+        secondValue = "";
+        displayCount = 1.5;
+    }
+    if(operaters.includes(a) && (displayCount >=3)){
+        finalNo = findValue(firstValue, secondValue , operator);
+        display.textContent = finalNo;
+        firstValue = finalNo;
+        operator = a;
+        secondValue = "";
+        displayCount = 2;
 
     }
 }
-function operation2(a){
-    if(allNo.includes(a)){
-        display.textContent = "ERROR"
+
+
+
+
+
+
+
+
+function findValue(firstNumber , SecondNumber , ope){
+   let firstVal = parseInt(firstNumber);
+   let secondVal = parseInt(SecondNumber);
+   
+    if (equalTo.includes(ope)){
+    result = (operate(firstVal , secondVal , operator))
+    
+    return result;
+
     }
+    if (operaters.includes(ope)){
+    
+    result = (operate(firstVal , secondVal , ope));
+   
+    return result;
+    }
+
 }
